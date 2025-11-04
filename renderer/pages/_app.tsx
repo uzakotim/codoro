c9489f6c3465b7b4e56c745c67f94d171be6c01e
uzakotim <timauzakov@gmail.com>
2025-11-04 13:19:48 +0500

    removed unncessary router in _app.tsx

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps } : AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const params = new URLSearchParams(window.location.search);
-      const popupPage = params.get('popup');
-      if (popupPage) {
-        router.replace(`/${popupPage}`);
-      }
-    }
-  }, [router]);
 
   return <Component {...pageProps} />;
 }
